fb39fe07ba5e8b0941212523ea41e754acdd4bb7
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-05-03 19:49:41 +0700

    Refactor tests

--- tests/gendiff/test_gendiff.py
+++ tests/gendiff/test_gendiff.py
@@ -7,38 +7,76 @@ TEST_FILES_DIR = os.path.join('tests', 'fixtures')
 
 
 @pytest.fixture
-def json_path_1():
-    return os.path.join(TEST_FILES_DIR, 'file1.json')
+def json_path_plain_1():
+    return os.path.join(TEST_FILES_DIR, 'file1_plain.json')
 
 
 @pytest.fixture
-def json_path_2():
-    return os.path.join(TEST_FILES_DIR, 'file2.json')
+def json_path_plain_2():
+    return os.path.join(TEST_FILES_DIR, 'file2_plain.json')
 
 
 @pytest.fixture
-def yaml_path_1():
-    return os.path.join(TEST_FILES_DIR, 'file1.yaml')
+def yaml_path_plain_1():
+    return os.path.join(TEST_FILES_DIR, 'file1_plain.yaml')
 
 
 @pytest.fixture
-def yaml_path_2():
-    return os.path.join(TEST_FILES_DIR, 'file2.yaml')
+def yaml_path_plain_2():
+    return os.path.join(TEST_FILES_DIR, 'file2_plain.yaml')
 
 
 @pytest.fixture
-def expected_diff():
-    path = os.path.join(TEST_FILES_DIR, 'expected.txt')
+def json_path_nested_1():
+    return os.path.join(TEST_FILES_DIR, 'file1_nested.json')
+
+
+@pytest.fixture
+def json_path_nested_2():
+    return os.path.join(TEST_FILES_DIR, 'file2_nested.json')
+
+
+@pytest.fixture
+def yaml_path_nested_1():
+    return os.path.join(TEST_FILES_DIR, 'file1_nested.yaml')
+
+
+@pytest.fixture
+def yaml_path_nested_2():
+    return os.path.join(TEST_FILES_DIR, 'file2_nested.yaml')
+
+
+@pytest.fixture
+def expected_diff_plain():
+    path = os.path.join(TEST_FILES_DIR, 'expected_plain.txt')
     with open(path) as file:
         expected = file.read()
     return expected
 
 
-def test_generate_diff_json(json_path_1, json_path_2, expected_diff):
-    diff = generate_diff(json_path_1, json_path_2)
-    assert diff == expected_diff
+@pytest.fixture
+def expected_diff_nested():
+    path = os.path.join(TEST_FILES_DIR, 'expected_nested.txt')
+    with open(path) as file:
+        expected = file.read()
+    return expected
 
 
-def test_generate_diff_yaml(yaml_path_1, yaml_path_2, expected_diff):
-    diff = generate_diff(yaml_path_1, yaml_path_2)
-    assert diff == expected_diff
+def test_generate_diff_plain_json(json_path_plain_1, json_path_plain_2, expected_diff_plain):
+    diff = generate_diff(json_path_plain_1, json_path_plain_2)
+    assert diff == expected_diff_plain
+
+
+def test_generate_diff_plain_yaml(yaml_path_plain_1, yaml_path_plain_2, expected_diff_plain):
+    diff = generate_diff(yaml_path_plain_1, yaml_path_plain_2)
+    assert diff == expected_diff_plain
+
+
+def test_generate_diff_nested_json(json_path_nested_1, json_path_nested_2, expected_diff_nested):
+    diff = generate_diff(json_path_nested_1, json_path_nested_2)
+    assert diff == expected_diff_nested
+
+
+def test_generate_diff_nested_yaml(yaml_path_nested_1, yaml_path_nested_2, expected_diff_nested):
+    diff = generate_diff(yaml_path_nested_1, yaml_path_nested_2)
+    assert diff == expected_diff_nested
