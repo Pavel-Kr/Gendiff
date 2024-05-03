da6468a189b69d990d2bd63114f6ccad54567f1f
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-05-03 21:14:39 +0700

    Refactoring of internal structure

--- gendiff/lib/formatters.py
+++ gendiff/lib/formatters.py
@@ -1,38 +1,56 @@
-def stylish(tree: dict) -> str:
-    spaces_per_indent = 4
-    offset_to_left = 2
+action_to_sign = {
+    'keep': ' ',
+    'remove': '-',
+    'add': '+'
+}
 
-    def stylish_value(value, depth) -> str:
+spaces_per_indent = 4
+
+
+def decorate_value(value, depth):
+    if isinstance(value, dict):
         current_indent = depth * spaces_per_indent
         deeper_indent = (depth + 1) * spaces_per_indent
+        strings = ['{']
+        spaces = ' ' * deeper_indent
+        for key, val in value.items():
+            if not isinstance(val, dict):
+                strings.append(f'{spaces}{key}: {val}')
+            else:
+                new_val = decorate_value(val, depth + 1)
+                strings.append(f'{spaces}{key}: {new_val}')
+        strings.append(' ' * current_indent + '}')
+        return '\n'.join(strings)
+    else:
+        return str(value)
 
-        if isinstance(value, dict):
-            strings = ['{']
-            for key, val in value.items():
-                spaces = ' ' * deeper_indent
-                strings.append(f'{spaces}{key}: {stylish_value(val, depth + 1)}')
-            spaces = ' ' * current_indent
-            strings.append(spaces + '}')
-            return '\n'.join(strings)
-        else:
-            return str(value)
+
+def stylish(tree: dict) -> str:
+    offset_to_left = 2
 
     def walk(diff: dict, depth):
         current_indent = depth * spaces_per_indent
         deeper_indent = (depth + 1) * spaces_per_indent
         diff_list = ['{']
         for key in sorted(diff.keys()):
-            for data in diff[key]:
-                if 'value' in data:
-                    action = data['action']
-                    value = data['value']
-                    spaces = ' ' * (deeper_indent - offset_to_left)
-                    diff_list.append(f'{spaces}{action} {key}: {stylish_value(value, depth + 1)}')
-                elif 'children' in data:
-                    children = data['children']
-                    spaces = ' ' * deeper_indent
-                    value = walk(children, depth + 1)
-                    diff_list.append(f'{spaces}{key}: {value}')
+            val = diff[key]
+            if 'action' in val.keys():
+                action = val['action']
+                spaces = ' ' * (deeper_indent - offset_to_left)
+                if action == 'changed':
+                    old_value = decorate_value(val['old_value'], depth + 1)
+                    new_value = decorate_value(val['new_value'], depth + 1)
+                    diff_list.append(f'{spaces}- {key}: {old_value}')
+                    diff_list.append(f'{spaces}+ {key}: {new_value}')
+                else:
+                    sign = action_to_sign[action]
+                    value = decorate_value(val['value'], depth + 1)
+                    diff_list.append(f'{spaces}{sign} {key}: {value}')
+            elif 'children' in val.keys():
+                children = val['children']
+                spaces = ' ' * deeper_indent
+                value = walk(children, depth + 1)
+                diff_list.append(f'{spaces}{key}: {value}')
         diff_list.append((' ' * current_indent) + '}')
         result = '\n'.join(diff_list)
         return result
--- gendiff/lib/gendiff.py
+++ gendiff/lib/gendiff.py
@@ -3,28 +3,24 @@ from gendiff.lib.config_parser import parse_file
 from gendiff.lib.formatters import stylish
 
 
-_KEEP = ' '
-_REMOVE = '-'
-_ADD = '+'
-
-
 def build_diff(config_1: dict, config_2: dict) -> dict:
-    diff = defaultdict(list)
+    diff = defaultdict()
     key_set_1 = set(config_1)
     key_set_2 = set(config_2)
     for key in key_set_1 - key_set_2:
-        diff[key].append({'action': _REMOVE, 'value': config_1[key]})
+        diff[key] = {'action': "remove", 'value': config_1[key]}
     for key in key_set_2 - key_set_1:
-        diff[key].append({'action': _ADD, 'value': config_2[key]})
+        diff[key] = {'action': "add", 'value': config_2[key]}
     for key in key_set_1 & key_set_2:
         if isinstance(config_1[key], dict) and isinstance(config_2[key], dict):
             nested_diff = build_diff(config_1[key], config_2[key])
-            diff[key].append({'children': nested_diff})
+            diff[key] = {'children': nested_diff}
         elif config_1[key] == config_2[key]:
-            diff[key].append({'action': _KEEP, 'value': config_1[key]})
+            diff[key] = {'action': "keep", 'value': config_1[key]}
         else:
-            diff[key].append({'action': _REMOVE, 'value': config_1[key]})
-            diff[key].append({'action': _ADD, 'value': config_2[key]})
+            diff[key] = {'action': "changed",
+                         'old_value': config_1[key],
+                         'new_value': config_2[key]}
     return diff
 
 
